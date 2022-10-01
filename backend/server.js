128a29b58daf5fc259921b6a87938bb0db77b194
Priscila <priscilapintado66@gmail.com>
2022-09-30 22:05:41 -0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+/*
+This is the entry point to my server 
+
+The root is where all my backend dependencies are going to go
+- dependencies: Express.js 
+- these dependencies are located in a folder called node modules in the root
+*/
+
+/* eslint-disable no-alert, no-console */
+console.log("Hello World");
